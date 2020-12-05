2d2207a3441df30552a1e379e2a5555835d51af3
Helen Xu <helen.z.xu@duke.edu>
2020-12-05 13:25:35 -0800

    html get request from search engine

--- EcoFind/popup.js
+++ EcoFind/popup.js
@@ -1,3 +1,32 @@
+// import './search.js';
+var results = [];
+function hndlr(response) {
+    document.getElementById("content").innerHTML += "<br/>" + "bye";
+    // for (var i = 0; i < response.items.length; i++) {
+    //     var item = response.items[i];
+    //     // in production code, item.htmlTitle should have the HTML entities escaped.
+    //     results.push(item.link);
+    //     //document.getElementById("content").innerHTML += "<br/>" + item.link;
+    // }
+    // for (var j=0; j<response.items.length; j++){
+    //     document.getElementById("content").innerHTML += "<br/>" + results[j];
+// }
+    document.getElementById("content").innerHTML += "<br/>" + response;
+    
+}
+function search(searchWords) {
+    //document.getElementById("content").innerHTML += "<br/>" + "bye";
+    
+    //"https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
+    const Http = new XMLHttpRequest();
+    const url = "https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
+    Http.open('GET',url);
+    Http.send();
+    Http.onreadystatechange=(e)=>{
+        //if (this.readyState===4)
+            hndlr(Http.responseText);
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function(){
 
@@ -6,16 +35,25 @@ document.addEventListener('DOMContentLoaded', function(){
         //find url and title of page
         tablink = tab.url;
         title = tab.title;
-        brand = brand.hostName;
+        document.getElementById("content").innerHTML += "<br/>" + title;
+        document.getElementById("content").innerHTML += "<br/>" + tablink;
+        //brand = brand.hostName;
 
         //print to chrome extension
         // document.write(tablink+"\n");
         // document.write(title);
-    });
-    var alternateItemsButton = document.getElementById('alternate');
-    alternateItemsButton.addEventListener('click', function(){
-        alert("These items are more sustainable\n"+tablink+"\n"+title+"\n"+brand);
 
-    }, false );
+        //alert(title);
+    });
+    // var alternateItemsButton = document.getElementById('alternate');
+    // alternateItemsButton.addEventListener('click', function(){
+    //     alert("These items are more sustainable\n"+tablink+"\n"+title+"\n"+brand);
+
+    // }, false );
+
+    document.getElementById("content").innerHTML += "<br/>" + "hi";
+    var searchWords = "blackdress";
+    search(searchWords);
+    document.getElementById("content").innerHTML += "<br/>" + "hi";
     
 }, false);
