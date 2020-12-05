b066ff00c5055aa5a185822f64d5c50f5605016a
Helen Xu <helen.z.xu@duke.edu>
2020-12-05 10:16:09 -0800

    added code to find url and title

--- EcoFind/popup.js
+++ EcoFind/popup.js
@@ -4,4 +4,13 @@ document.addEventListener('DOMContentLoaded', function(){
         alert("These items are more sustainable");
 
     }, false );
+    chrome.tabs.getSelected(null,function(tab) {
+        //find url and title of page
+        var tablink = tab.url;
+        var title = tab.title;
+
+        //print to chrome extension
+        document.write(tablink+"\n");
+        document.write(title);
+    });
 }, false);
