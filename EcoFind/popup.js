6b7e2840635dfc89fb9b692093fa0b8ba4dde84e
Helen Xu <helen.z.xu@duke.edu>
2020-12-05 10:24:29 -0800

    fixed display formatting

--- EcoFind/popup.js
+++ EcoFind/popup.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var alternateItemsButton = document.getElementById('alternate');
-    alternateItemsButton.addEventListener('click', function(){
-        alert("These items are more sustainable");
-
-    }, false );
+    var title, tablink;
     chrome.tabs.getSelected(null,function(tab) {
         //find url and title of page
-        var tablink = tab.url;
-        var title = tab.title;
+        tablink = tab.url;
+        title = tab.title;
 
         //print to chrome extension
-        document.write(tablink+"\n");
-        document.write(title);
+        // document.write(tablink+"\n");
+        // document.write(title);
     });
+    var alternateItemsButton = document.getElementById('alternate');
+    alternateItemsButton.addEventListener('click', function(){
+        alert("These items are more sustainable\n"+tablink+"\n"+title);
+
+    }, false );
+    
 }, false);
