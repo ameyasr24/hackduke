3bea3bf0fb6cf18c8d867be51137469167329066
Gargi Mahadeshwar <gargi.mahadeshwar@duke.edu>
2020-12-05 12:29:19 -0500

    basic extension

---
+++ EcoFind/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var alternateItemsButton = document.getElementById('alternate');
+    alternateItemsButton.addEventListener('click', function(){
+        alert("These items are more sustainable");
+
+    }, false );
+}, false);
