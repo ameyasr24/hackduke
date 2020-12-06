2eeae0b4bb534e78e9b6e4c20dc0edfa923c0568
Helen Xu <helen.z.xu@duke.edu>
2020-12-05 17:07:39 -0800

    select for title, image, and link of recommended products

--- EcoFind/popup.js
+++ EcoFind/popup.js
@@ -1,31 +1,64 @@
-// import './search.js';
+
 var results = [];
+var count = 0;
 function hndlr(response) {
-    document.getElementById("content").innerHTML += "<br/>" + "bye";
-    // for (var i = 0; i < response.items.length; i++) {
-    //     var item = response.items[i];
-    //     // in production code, item.htmlTitle should have the HTML entities escaped.
-    //     results.push(item.link);
-    //     //document.getElementById("content").innerHTML += "<br/>" + item.link;
-    // }
-    // for (var j=0; j<response.items.length; j++){
-    //     document.getElementById("content").innerHTML += "<br/>" + results[j];
-// }
-    document.getElementById("content").innerHTML += "<br/>" + response;
-    
+    // var formatted = response.substring(response.indexOf("items")-1);
+    // var jsonResponse = JSON.parse(formatted);
+    if (response && count<1) {
+        count++;
+        var index = response.indexOf("items");
+        var remove = response.substring(0,index);
+        var result = response.replaceAll(remove,"");
+        index = 0;
+        for (var i=0;i<3;i++) {
+            var info = [];
+
+            // in production code, item.htmlTitle should have the HTML entities escaped.
+            //results.push(item.link);
+
+            //find title
+            var startIndex = result.indexOf('"title"',index)+10;
+            //index = startIndex+1;
+            var endIndex = result.indexOf('",',startIndex+1);
+            var title = result.substring(startIndex,endIndex);
+            info.push(title);
+
+            //find image
+            startIndex = result.indexOf('"image":',index)+10;
+            endIndex = result.indexOf('",',startIndex+1);
+            var picture = result.substring(startIndex,endIndex);
+            info.push(picture);
+
+            //find link
+            startIndex = result.indexOf('"link"',index)+9;
+            //index = startIndex+1;
+            endIndex = result.indexOf('",',startIndex+1);
+            var link = result.substring(startIndex,endIndex);
+            info.push(link);
+
+            index = endIndex;
+
+            //add to results
+            results.push(info);
+        }
+        document.getElementById("col1").innerHTML += results[0];
+        document.getElementById("col2").innerHTML += results[1];
+        document.getElementById("col3").innerHTML += results[2];
+        // for (var j=0; j<results.length; j++) {
+        //     document.getElementById("content").innerHTML += "<br/>" + results[j];
+        // }
+    }
 }
-function search(searchWords) {
-    //document.getElementById("content").innerHTML += "<br/>" + "bye";
-    
+function search(searchWords) {    
     //"https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
     const Http = new XMLHttpRequest();
-    const url = "https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
+    const url = "https://www.googleapis.com/customsearch/v1?key={APIkey}&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
     Http.open('GET',url);
     Http.send();
     Http.onreadystatechange=(e)=>{
-        //if (this.readyState===4)
             hndlr(Http.responseText);
     }
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -35,8 +68,8 @@ document.addEventListener('DOMContentLoaded', function(){
         //find url and title of page
         tablink = tab.url;
         title = tab.title;
-        document.getElementById("content").innerHTML += "<br/>" + title;
-        document.getElementById("content").innerHTML += "<br/>" + tablink;
+        document.getElementById("productName").innerHTML += "<br/>" + title;
+        //document.getElementById("content").innerHTML += "<br/>" + tablink+"<br/>";
         //brand = brand.hostName;
 
         //print to chrome extension
@@ -51,9 +84,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // }, false );
 
-    document.getElementById("content").innerHTML += "<br/>" + "hi";
+    //document.getElementById("content").innerHTML += "<br/>" + "hi";
     var searchWords = "blackdress";
     search(searchWords);
-    document.getElementById("content").innerHTML += "<br/>" + "hi";
+    //document.getElementById("content").innerHTML += "<br/>" + "hi";
     
 }, false);
