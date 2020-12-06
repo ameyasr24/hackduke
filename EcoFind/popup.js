593a9ab735de136fe456d3dde8e7737376afd9c7
Helen Xu <helen.z.xu@duke.edu>
2020-12-06 10:13:07 -0800

    deleted unnecessary code

--- EcoFind/popup.js
+++ EcoFind/popup.js
@@ -46,16 +46,12 @@ function hndlr(response) {
 
         //add to HTML in proper formatting
         for (var i=1;i<=3;i++) {
-            //var id = "col"+i;
             var imgID = "img"+i;
             var infoID = "info"+i;
             for (var j=0;j<results[i-1].length;j++) {
                 switch (results[i-1][j].charAt(0)) {
                     //title
                     case 't':
-                        // var newHeading = document.createElement("h4");
-                        // newHeading.innerHTML = (results[i-1][j].substring(7).replaceAll('\\"','\"'));
-                        //document.getElementById(id).appendChild(newHeading);
                         document.getElementById(infoID).innerHTML = results[i-1][j].substring(7).replaceAll('\\"','\"');
                         break;
 
@@ -63,19 +59,12 @@ function hndlr(response) {
                     case 'i':
                         var img = document.createElement("img");
                         img.src = results[i-1][j].substring(7);
-                        // document.getElementById(id).appendChild(img);
                         document.getElementById(imgID).appendChild(img);
                         break;
 
                     //link
                     case 'l':
                         document.getElementById(infoID).setAttribute('href',results[i-1][j].substring(6));
-                        // var a = document.createElement('a');
-                        // a.setAttribute('href',results[i-1][j].substring(6));
-                        // a.setAttribute('target',"_blank");
-                        //a.innerHTML = "Link here!";
-                        // document.getElementById(id).appendChild(a);
-                        // document.getElementById(infoID).appendChild(a);
                         break;
                 }
             }
@@ -85,7 +74,7 @@ function hndlr(response) {
 
 function search(searchWords) {
     //change APIkey to own key
-    const APIKey = {ReplaceAPIKey};
+    const APIKey = "Your-API-Key";
     const Http = new XMLHttpRequest();
     const url = "https://www.googleapis.com/customsearch/v1?key="+APIKey+"&cx=c391cddd12bed7ac3&q=" + searchWords + "&callback=hndlr";
     Http.open('GET',url);
@@ -158,13 +147,5 @@ document.addEventListener('DOMContentLoaded', function(){
         search(title);
         rating(title);
         getRating("https://directory.goodonyou.eco/brand/abercrombie-and-fitch");
-        //document.getElementById("content").innerHTML += "<br/>" + tablink+"<br/>";
-        //brand = brand.hostName;
-
-        //alert(title);
     });
-    // var alternateItemsButton = document.getElementById('alternate');
-    // alternateItemsButton.addEventListener('click', function(){
-    //     alert("These items are more sustainable\n"+tablink+"\n"+title+"\n"+brand);
-    // }, false );
 }, false);
